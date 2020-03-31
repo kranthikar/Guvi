e602150f439c55674213403463521769203b23d3
kranthikar <62943282+kranthikar@users.noreply.github.com>
2020-03-31 20:08:18 +0530

    767.py

---
+++ python/767.py
@@ -0,0 +1,26 @@
+'''You are given three numbers A, B & C. Print the largest amongst these three numbers.
+
+Input Description:
+Three numbers are provided to you.
+
+Output Description:
+Find and print the largest among the three
+
+Sample Input :
+1
+2
+3
+Sample Output :
+3'''
+
+ code
+ 
+ A=int(input())
+B=int(input())
+C=int(input())
+if(A>B and A>C):
+         print(A)
+elif B>C:
+         print(B)
+else:
+         print(C)
