076ac5e02e3d325a837282e07185ca9dabe370a9
kranthikar <62943282+kranthikar@users.noreply.github.com>
2020-04-01 08:20:09 +0530

    322.py

---
+++ basics/322.py
@@ -0,0 +1,16 @@
+'''Given a string S consisting of 2 words reverse the order of two words .
+Input Size : |S| <= 10000000
+Sample Testcase :
+INPUT
+hello world
+OUTPUT
+world hello'''
+
+
+x = str(input())
+a = x.split()
+a.reverse()
+print(' '.join(a))
+
+
+
